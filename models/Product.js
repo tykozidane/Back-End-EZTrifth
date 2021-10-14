44758db68abee220bc2ad57370e9178aaae024fe
tykozidane2015 <tykozidane2015@gmail.com>
2021-10-14 20:49:32 +0700

    First Push

---
+++ models/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    desc: { type: String, required: true, },
+    img: { type: String, required: true },
+    categories: { type: Array },
+    size: { type: String },
+    color: { type: String },
+    price: { type: Number, required: true },
+    status: { type: String, default: "pending"}
+    
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
