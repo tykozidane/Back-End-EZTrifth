bc1d4623d3390959d9d2d10711121d5f2380f2c2
tykozidane2015 <tykozidane2015@gmail.com>
2021-12-23 20:30:05 +0700

    User

--- models/User.js
+++ models/User.js
@@ -1,21 +1,24 @@
-const mongoose = require("mongoose")
-const { boolean } = require("webidl-conversions")
+const mongoose = require("mongoose");
+const { boolean } = require("webidl-conversions");
 
-const userSchema = new mongoose.Schema({
-    username: {type: String, required:true, unique:true},
-    namalengkap: {type: String, default: " "},
-    email: {type: String, required:true, unique:true},
-    password: {type: String, required:true},
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    namalengkap: { type: String, default: " " },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     isAdmin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-    notelp: {type: String, default: " "},
-    kota: {type: String, default: " "},
-    provinsi: {type: String, default: " "},
-    alamat: {type: String, default: " "},
-    img: {type: String, default: " "},
-    
-},{ timestamps: true });
+    notelp: { type: String, default: " " },
+    kota: { type: String, default: " " },
+    provinsi: { type: String, default: " " },
+    alamat: { type: String, default: " " },
+    img: { type: String, default: " " },
+    ktp: { type: String, default: " " },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
